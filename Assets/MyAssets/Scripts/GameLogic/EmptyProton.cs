c1d4ab4131f956f3fb053f97b5ee52ae8090f9fb
Otju <oleg.sid.14@gmail.com>
2023-12-10 18:13:34 +0200

    levels

---
+++ Assets/MyAssets/Scripts/GameLogic/EmptyProton.cs
@@ -0,0 +1,18 @@
+﻿using MyAssets.Scripts.Data;
+using UnityEngine;
+using Zenject;
+
+namespace MyAssets.Scripts.GameLogic
+{
+    public class EmptyProton: MonoBehaviour
+    {
+        [SerializeField] protected SpriteRenderer _sr;
+        
+        [Inject]
+        public virtual void Construct(GameData gameData)
+        {
+            _sr.sprite = gameData.Skins.CurrentSkin.ProtonSprite;
+            _sr.color = gameData.Skins.CurrentSkin.ProtonColor;
+        }
+    }
+}
